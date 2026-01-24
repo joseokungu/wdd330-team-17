6733fde34e6e513a929a90a6352293ee4980b73c
YeiriMtz <yeirimartinez80@gmail.com>
2026-01-23 21:54:45 -0600

    Fix Vite build configuration for Render

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,21 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src/',
+  root: 'src',
 
   build: {
-  outDir: "../dist",
-  rollupOptions: {
-    input: {
-      main: resolve(__dirname, "src/index.html"),
-      cart: resolve(__dirname, "src/cart/index.html"),
-      checkout: resolve(__dirname, "src/checkout/index.html"),
-      product: resolve(__dirname, "src/product_pages/index.html"
-      ),
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        cart: resolve(__dirname, 'src/cart/index.html'),
+        checkout: resolve(__dirname, 'src/checkout/index.html'),
 
-    },
-  },
-},
+        cedar: resolve(__dirname, 'src/product_pages/cedar-ridge-rimrock-2.html'),
+        marmot: resolve(__dirname, 'src/product_pages/marmot-ajax-3.html'),
+        alpine: resolve(__dirname, 'src/product_pages/northface-alpine-3.html'),
+        talus: resolve(__dirname, 'src/product_pages/northface-talus-4.html')
+      }
+    }
+  }
 });
