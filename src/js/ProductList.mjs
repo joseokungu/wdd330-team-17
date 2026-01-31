ac419c39b1480b63059ee4d9663c861465a0135e
YeiriMtz <yeirimartinez80@gmail.com>
2026-01-31 17:02:23 -0600

    .

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,15 +1,38 @@
+function productCardTemplate(product) {
+  return `
+    <li class="product-card">
+      <a href="product_pages/?product=${product.Id}">
+        <img src="${product.Image}" alt="Image of ${product.Name}">
+        <h3 class="card__brand">${product.Brand}</h3>
+        <h2 class="card__name">${product.Name}</h2>
+        <p class="product-card__price">$${product.Price}</p>
+      </a>
+    </li>
+  `;
+}
+
 export default class ProductList {
   constructor(category, dataSource, listElement) {
-    // You passed in this information to make the class as reusable as possible.
-    // Being able to define these things when you use the class will make it very flexible
     this.category = category;
     this.dataSource = dataSource;
     this.listElement = listElement;
   }
 
   async init() {
-    // the dataSource will return a Promise...so you can use await to resolve it.
-    const list = await this.dataSource.getData();
-    // next, render the list – ** future **
-  }
+  const list = await this.dataSource.getData();
+  this.renderList(list);
 }
+}
+
+renderList(list) {
+  renderListWithTemplate(
+    productCardTemplate,
+    this.listElement,
+    list,
+    'afterbegin',
+    true
+  );
+}
+
+import { renderListWithTemplate } from './utils.mjs';
+
